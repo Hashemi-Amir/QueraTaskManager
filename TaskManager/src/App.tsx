7c76f639ec53dde624a0c44a975d7322a4f096d7
Saeed-Abedini <Saeed78Abedini@gmail.com>
2023-05-16 23:39:00 +0330

    initial commit

---
+++ TaskManager/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const style = 'flex justify-center items-center flex-col h-full items-center flex-col h-fullitems-center flex-col h-full'
+
+function App() {
+  return (
+    <div className={style}>
+      <h1 className="text-gray-400 text-4xl">Hello React</h1>
+      <h1 className="text-gray-950 text-4xl">Hello React</h1>
+      #78C6B0
+    </div>
+  );
+}
+
+export default App;
