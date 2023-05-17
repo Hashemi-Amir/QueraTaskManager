7abbe26cd7a057fe743e2a587c52e2befaad56ab
amir996 <amir2041375@gmail.com>
2023-05-17 14:40:18 +0330

    Adding Input component

---
+++ TaskManager/src/components/Input/Input.tsx
@@ -0,0 +1,19 @@
+type InputProps = { label: string } & React.ComponentProps<"input">;
+
+function Input({ label, ...rest }: InputProps) {
+  return (
+    <div>
+      <label htmlFor={rest.id} className=" text-sm font-normal text-black">
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          {...rest}
+          className="w-full border border-[#AAAAAA] h-10 rounded-md px-3 py-2 focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Input;
