ed86fef00f80e54d315c0862b8ec43ea8d5735dc
amir996 <amir2041375@gmail.com>
2023-05-19 23:38:04 +0330

    configure routing

--- TaskManager/src/App.tsx
+++ TaskManager/src/App.tsx
@@ -1,8 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import { Login } from "./pages/Login";
+import Register from "./pages/Register";
+import { ResetPassword } from "./pages/Reset";
+import { Forget } from "./pages/Forget";
 function App() {
   return (
-    <div className="flex justify-center">
-      <h1 className="text-4xl">Hello React</h1>
-    </div>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="reset" element={<ResetPassword />} />
+      <Route path="forget" element={<Forget />} />
+    </Routes>
   );
 }
 
