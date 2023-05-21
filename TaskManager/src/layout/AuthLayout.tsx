cd9cf56d00f9d0ad02d40e26a8b69d2d70c54b10
amir996 <amir2041375@gmail.com>
2023-05-21 11:19:35 +0330

    Adding routing and fixed the dynamic signed up or not signed up message to the layout

--- TaskManager/src/layout/AuthLayout.tsx
+++ TaskManager/src/layout/AuthLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Button from "../components/ui/Button";
+import { useLocation, useNavigate } from "react-router-dom";
 
 type layout = {
   children: ReactNode;
@@ -7,6 +8,22 @@ type layout = {
 };
 
 const AuthLayout = ({ children, BtnValue }: layout) => {
+  const Navigate = useNavigate();
+  const Location = useLocation();
+
+  // Getting the right message to show to the user based on the current route
+  const signUpMessage =
+    Location.pathname === "/login" || Location.pathname === "/"
+      ? "ثبت نام نکرده ای؟"
+      : "قبلا ثبت نام کرده‌ای؟";
+
+  // Getting the right route to navigate based on the current route
+  const NavigateTo =
+    Location.pathname === "/login" || Location.pathname === "/"
+      ? "/register"
+      : "/login";
+
+  console.log(Location.pathname);
   const authLayoutWrapper =
     "w-screen h-screen flex flex-col justify-center items-center";
   const authLayoutHeader =
@@ -24,9 +41,9 @@ const AuthLayout = ({ children, BtnValue }: layout) => {
         </div>
 
         <div className={authLayoutHeaderRegisterOffer}>
-          <p className="flex-shrink-0">قبلا ثبت نام کرده‌ای؟</p>
+          <p className="flex-shrink-0">{signUpMessage}</p>
 
-          <Button value={BtnValue} />
+          <Button onClick={() => Navigate(NavigateTo)} value={BtnValue} />
         </div>
       </div>
       <div className="w-full h-1/2 absolute  top-1/2 -z-10">
