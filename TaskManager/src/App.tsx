8ca88ff4fe6c0c11e1287e61d9cbdd89f430625f
amir996 <amir2041375@gmail.com>
2023-05-21 12:42:14 +0330

    Adding 404 page route to our routing system

--- TaskManager/src/App.tsx
+++ TaskManager/src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Reset from "./pages/Reset";
 import Forgot from "./pages/Forgot";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <Routes>
@@ -11,6 +12,7 @@ function App() {
       <Route path="register" element={<Register />} />
       <Route path="reset" element={<Reset />} />
       <Route path="forgot" element={<Forgot />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
