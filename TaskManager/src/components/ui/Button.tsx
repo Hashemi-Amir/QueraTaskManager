bbc1626772664d4398b941ae4a496f1b7f61dccb
amir996 <amir2041375@gmail.com>
2023-05-21 11:12:48 +0330

    fixing Type Aliases of the Button component in order to be able to use button attributes in the Button component

--- TaskManager/src/components/ui/Button.tsx
+++ TaskManager/src/components/ui/Button.tsx
@@ -1,11 +1,15 @@
 type ButtonProps = {
   value: string;
-};
+} & React.ComponentProps<"button">;
 
-const Button = (props: ButtonProps) => {
+const Button = ({ value, ...rest }: ButtonProps) => {
   const style =
-    "w-full h-10 p-2.5 gap-2.5 text-sm font-bold leading-4 flex justify-center items-center text-white rounded-md bg-green-15 ";
-  return <button className={style}>{props.value}</button>;
+    "w-full h-10 p-2.5 gap-2.5 text-sm font-bold leading-4 flex justify-center items-center text-white rounded-md bg-green-700 ";
+  return (
+    <button {...rest} className={style}>
+      {value}
+    </button>
+  );
 };
 
 export default Button;
