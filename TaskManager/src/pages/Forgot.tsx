71aee5f85cbd640d7ac933a727dd80f675af9f90
amir996 <amir2041375@gmail.com>
2023-05-21 11:28:51 +0330

    Renaming Forgot page

--- TaskManager/src/pages/Forgot.tsx
+++ TaskManager/src/pages/Forgot.tsx
@@ -1,14 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../components/ui/Button";
 import Card from "../components/ui/Card";
 import Input from "../components/ui/Input";
 import AuthLayout from "../layout/AuthLayout";
 
 const Forget = () => {
+  const Navigate = useNavigate();
   return (
     <AuthLayout BtnValue="ورود">
       <Card cardTitle="فراموشی رمز عبور">
         <Input label="ایمیل خود را وارد کنید" />
-        <Button value="دریافت ایمیل بازیابی رمز عبور" />
+        <Button
+          onClick={() => Navigate("/reset")}
+          value="دریافت ایمیل بازیابی رمز عبور"
+        />
       </Card>
     </AuthLayout>
   );
