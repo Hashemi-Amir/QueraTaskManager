51f3d9353c03087d4eb636542010410e38b56ed8
amir996 <amir2041375@gmail.com>
2023-05-21 11:22:42 +0330

    Modifying routing and fixed some typos

--- TaskManager/src/App.tsx
+++ TaskManager/src/App.tsx
@@ -2,14 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Reset from "./pages/Reset";
-import Forget from "./pages/Forget";
+import Forgot from "./pages/Forgot";
 function App() {
   return (
     <Routes>
+      <Route index path="/" element={<Login />} />
       <Route path="login" element={<Login />} />
       <Route path="register" element={<Register />} />
       <Route path="reset" element={<Reset />} />
-      <Route path="forget" element={<Forget />} />
+      <Route path="forgot" element={<Forgot />} />
     </Routes>
   );
 }
