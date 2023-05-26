1a05b3a67fc502e68d648f739b438854421c56dd
amir996 <amir2041375@gmail.com>
2023-05-26 12:34:29 +0330

    creating TaskInfoCard, Comment and TaskInfo components

---
+++ TaskManager/src/components/TaskInformation/TaskInfoCard.tsx
@@ -0,0 +1,17 @@
+interface TaskInfoCardProps {
+  children: React.ReactNode;
+  // onClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+}
+const TaskInfoCard = ({ children }: TaskInfoCardProps) => {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-30 ">
+      <div className=" fixed top-0 left-0 flex items-center justify-center z-20 w-full h-full">
+        <div className="w-11/12 h-3/4 bg-white rounded-2xl p-8 relative overflow-hidden">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskInfoCard;
