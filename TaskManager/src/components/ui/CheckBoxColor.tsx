62455e1b7027f91fc90e2febf7cb19cd1df00790
ali.hy <alihayati511@gmail.com>
2023-05-26 17:48:23 +0330

    build add newTask modal

--- TaskManager/src/components/ui/CheckBoxColor.tsx
+++ TaskManager/src/components/ui/CheckBoxColor.tsx
@@ -8,7 +8,7 @@ const CheckBoxColor = ({id , className}:CheckBoxColorProps) => {
     
     return (
         <li 
-            className={`h-4 w-4 mr-3 rounded-sm hover:cursor-pointer ${className}`}
+            className={`h-4 w-4 mr-3 list-none rounded-sm hover:cursor-pointer ${className}`}
             id={id}
         ></li>
 
--- TaskManager/src/pages/Modals.tsx
+++ TaskManager/src/pages/Modals.tsx
@@ -1,6 +1,7 @@
 import {createPortal} from 'react-dom'
-import CardMedium from '../components/modals/Medium/CardMedium';
-import ShareModal from '../components/modals/Medium/ShareModal';
+import AddNewTask from '../components/modals/AddNewTask/AddNewTask'
+// import CardMedium from '../components/modals/Medium/CardMedium';
+// import ShareModal from '../components/modals/Medium/ShareModal';
 // import NewWorkspaceOne from '../components/modals/Medium/NewWorkspaceOne';
 // import NewWorkspaceTwo from '../components/modals/Medium/NewWorkspaceTwo';
 // import NewWorkspaceThree from '../components/modals/Medium/NewWorkspaceThree';
@@ -13,11 +14,7 @@ const Modals = () => {
             <h2>this is Modal page</h2>
             
             {createPortal(
-                <CardMedium 
-                   cardTitle='ساختن ورک اسپیس جدید'
-                >
-                    <ShareModal />
-                </CardMedium> ,
+                <AddNewTask />,
                 document.body
             )}
         </div>
