ba97f7694b9c3cff824a8c8d1b35a3795eb52e76
amir996 <amir2041375@gmail.com>
2023-06-01 20:00:10 +0330

    Adjust spacing to prevent new task button from overlaping boards and calendar cells

--- TaskManager/src/components/dashboard/dashboardSidebar/NewSpace.tsx
+++ TaskManager/src/components/dashboard/dashboardSidebar/NewSpace.tsx
@@ -2,7 +2,7 @@ import { CgAddR } from "react-icons/cg";
 
 const NewSpace = () => {
   return (
-    <button className="flex justify-center items-center gap-1 py-3 rounded-md bg-D3D3D3 text-xs font-semibold hover:bg-gradient-to-r from-118C80 to-4AB7D8">
+    <button className="flex justify-center items-center gap-1 py-3 rounded-md bg-D3D3D3 text-xs font-semibold hover:bg-gradient-to-r from-118C80 to-4AB7D8 hover:text-white">
       <CgAddR className="w-4 h-4" />
       ساختن اسپیس جدید
     </button>
