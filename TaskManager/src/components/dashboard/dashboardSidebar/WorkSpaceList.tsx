68fb5400f869afb1a7720962fbf3d75344abba9a
ali.hy <alihayati511@gmail.com>
2023-06-05 17:25:34 +0330

    change workspace more position

--- TaskManager/src/components/dashboard/dashboardSidebar/WorkSpaceList.tsx
+++ TaskManager/src/components/dashboard/dashboardSidebar/WorkSpaceList.tsx
@@ -11,22 +11,37 @@ type WorkSpaceProps = {
 };
 
 const WorkSpaceList = ({ spaceList }: WorkSpaceProps) => {
-  const [workspaceMore , setWorkspaceMore] = useState(false)
+  const [workspaceMore , setWorkspaceMore] = useState(null)
+
+  const handleItemClick = (item:any) => {
+    setWorkspaceMore(item)
+  }
+  const closeWorkSpace = () => {
+    setWorkspaceMore(null)
+  }
+  // const handleWorkSpaceMore = () => {
+  //   console.log('wop');
+    
+  //   setWorkspaceMore(!workspaceMore)
+  // }
   return (
-    <div className="my-5 flex-1 overflow-auto scrollbar-thin scrollbar-thumb-gray-200 scrollbar-thumb-rounded-full ">
+    <div className="my-5 relative flex-1 overflow-auto scrollbar-thin scrollbar-thumb-gray-200 scrollbar-thumb-rounded-full ">
       {spaceList.map((space) => {
         return (
           <div className="collapse" key={space.spaceName}>
             <input type="checkbox" className="p-0 m-0" />
             <div className="relative collapse-title font-medium flex justify-between items-center gap-2 p-0 m-0">
-              <div>
-                <span className={`w-5 h-5 rounded ${space.spaceColor}`} />
+              <div className="flex items-center">
+                <span className={`block w-5 h-5 ml-2 rounded ${space.spaceColor}`} />
                 {space.spaceName}
               </div>
 
-              <span className="absolute left-0 cursor-pointer" onClick={()=> setWorkspaceMore(true)}><BsThreeDots /></span>
-              {workspaceMore && <SideMore sideMoreState="ورک اسپیس"/>}
+              <span className="absolute z-50 left-0 block cursor-pointer" onClick={()=> handleItemClick(space.spaceName)}><BsThreeDots /></span>
+             
+              
             </div>
+           
+
             {space.projectName && (
               <div className="collapse-content  ">
                 {space.projectName.map((project) => (
@@ -37,9 +52,13 @@ const WorkSpaceList = ({ spaceList }: WorkSpaceProps) => {
                 ))}
               </div>
             )}
+            
+
           </div>
         );
       })}
+       {workspaceMore && <SideMore sideMoreState="ورک اسپیس"/>}
+
     </div>
   );
 };
