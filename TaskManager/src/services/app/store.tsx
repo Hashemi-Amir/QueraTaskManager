4aba921c8bc33efb1547f427e3644f92dd68366d
amir996 <amir2041375@gmail.com>
2023-06-09 15:15:32 +0330

    add auth slice to store and fixing some bugs

--- TaskManager/src/services/app/store.tsx
+++ TaskManager/src/services/app/store.tsx
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import calendarReducer from "../features/calendar/calendarSlice";
 import authReducer from "../features/auth/authSlice";
+import getUserSlice from "../features/getUser/getUserSlice";
+
+//! We should first import our actions from our slices here, then export them at the bottom
+import { getUser, reset } from "../features/getUser/getUserSlice";
 
 export type TypeStore = {
   calendar: {
@@ -12,6 +16,8 @@ const store = configureStore({
   reducer: {
     calendar: calendarReducer,
     auth: authReducer,
+    // !! this redcuder is for experiment I will remove it myslef
+    user: getUserSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
@@ -25,3 +31,6 @@ export default store;
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
+
+//! Export those imported actions here one by one
+export { getUser, reset };
