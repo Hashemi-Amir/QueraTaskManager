941cb7760b36ef91c9ec12ce5af2b2ae3373ca10
Saeed-Abedini <Saeed78Abedini@gmail.com>
2023-06-12 17:09:41 +0330

    optimize fetch reauests for boards and add in board drag and drop

--- TaskManager/src/components/dashboard/dashboardColumnView/TaskCard.tsx
+++ TaskManager/src/components/dashboard/dashboardColumnView/TaskCard.tsx
@@ -3,8 +3,7 @@ import { CiTextAlignRight } from "react-icons/ci";
 import ProfileButton from "../../ui/ProfileButton";
 import { FiCheckCircle, FiFlag } from "react-icons/fi";
 import { BsThreeDots } from "react-icons/bs";
-import { useSortable } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
+import { Draggable } from "react-beautiful-dnd";
 
 type Task = {
   _id: string;
@@ -19,7 +18,7 @@ type Task = {
   // deadline?: string;
 };
 
-const TaskCard = ({ name, description, _id }: Task) => {
+const TaskCard = ({ name, description, _id, position,comments }: Task) => {
   const [isExpanded, setIsExpanded] = useState(false);
   // const [colMoreModal , setColMoreModal] = useState(false)
 
@@ -27,88 +26,79 @@ const TaskCard = ({ name, description, _id }: Task) => {
     setIsExpanded(isHovering);
   };
 
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-    isDragging,
-  } = useSortable({ id: _id });
 
-  const style = {
-    transform: CSS.Translate.toString(transform),
-    transition,
-    cursor: isDragging ? "grabbing" : "pointer",
-  } as React.CSSProperties;
 
   return (
-    <div
-      className={`border w-[250px] rounded p-3 bg-white text-1E1E1E shadow-[0px_6px_8px_rgba(0,0,0,0.14)] mb-3`}
-      ref={setNodeRef}
-      {...attributes}
-      {...listeners}
-      style={style}
-      onMouseOver={() => handleCardHover(true)}
-      onMouseLeave={() => handleCardHover(false)}
-    >
-      <div className="flex justify-between items-baseline mb-2">
-        <div className="h-4 font-medium leading-4 text-right text-534D60 text-[10px]">
-          {name}
-        </div>
+    <Draggable draggableId={_id} index={position - 1}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          className={`border w-[250px] rounded p-3 bg-white text-1E1E1E shadow-[0px_6px_8px_rgba(0,0,0,0.14)] mb-3`}
+          onMouseOver={() => handleCardHover(true)}
+          onMouseLeave={() => handleCardHover(false)}
+        >
+          <div className="flex justify-between items-baseline mb-2">
+            <div className="h-4 font-medium leading-4 text-right text-534D60 text-[10px]">
+              {name}
+            </div>
 
-        <ProfileButton
-          abbreviation="NM"
-          className={`w-6 h-6 pt-[3px] text-[10px] ${
-            isExpanded ? "visible" : "invisible"
-          }
+            <ProfileButton
+              className={`w-6 h-6 pt-[3px] text-[10px] ${
+                isExpanded ? "visible" : "invisible"
+              }
             `}
-        />
-      </div>
-      <div className="flex items-center justify-start mb-5 gap-1">
-        <div className="font-medium text-xs text-0E0E0E leading-4 text-right">
-          {description}
-        </div>
-        <div className="text-xs text-BDC0C6 ">
-          <CiTextAlignRight />
-        </div>
-      </div>
-      <div className="flex justify-start items-center pb-5 text-[10px] gap-1">
-        <span className="text-FB0606">
-          <FiFlag />
-        </span>
-        <div className="text-343434">۵ مهر - فردا</div>
-        <span className="text-BDC0C6">
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <input
-                type="checkbox"
-                className="checkbox checkbox-success w-3 h-3 mb-[2px]"
-              />
-            </label>
+            />
           </div>
-        </span>
-        <div className="text-BDC0C6">۲۳/۲</div>
-      </div>
-      <div className="flex gap-3">
-        <span className="bg-BFFDE3 text-[10px] p-1 rounded-l-2xl">درس</span>
-        <span className="bg-EEDFF6 text-[10px] p-1 rounded-l-2xl">پروژه</span>
-      </div>
-      <div
-        className={` overflow-hidden justify-between flex border-t  transition-all duration-500 
+          <div className="flex items-center justify-start mb-5 gap-1">
+            <div className="font-medium text-xs text-0E0E0E leading-4 text-right">
+              {description}
+            </div>
+            <div className="text-xs text-BDC0C6 ">
+              <CiTextAlignRight />
+            </div>
+          </div>
+          <div className="flex justify-start items-center pb-5 text-[10px] gap-1">
+            <span className="text-FB0606">
+              <FiFlag />
+            </span>
+            <div className="text-343434">۵ مهر - فردا</div>
+            <span className="text-BDC0C6">
+              <div className="form-control">
+                <label className="cursor-pointer label">
+                  <input
+                    type="checkbox"
+                    className="checkbox checkbox-success w-3 h-3 mb-[2px]"
+                  />
+                </label>
+              </div>
+            </span>
+            <div className="text-BDC0C6">۲۳/۲</div>
+          </div>
+          <div className="flex gap-3">
+            <span className="bg-BFFDE3 text-[10px] p-1 rounded-l-2xl">درس</span>
+            <span className="bg-EEDFF6 text-[10px] p-1 rounded-l-2xl">
+              پروژه
+            </span>
+          </div>
+          <div
+            className={` overflow-hidden justify-between flex border-t  transition-all duration-500 
         ${isExpanded ? "h-9 mt-5 pt-4" : "h-[0px] opacity-0 mt-0 pt-0"}
         `}
-      >
-        <div className="hover:text-208D8E hover:scale-110">
-          <FiCheckCircle />
-        </div>
-        <div className="hover:scale-110">
-          <BsThreeDots />
+          >
+            <div className="hover:text-208D8E hover:scale-110">
+              <FiCheckCircle />
+            </div>
+            <div className="hover:scale-110">
+              <BsThreeDots />
 
-          {/* {colMoreModal && <ColMore />} */}
+              {/* {colMoreModal && <ColMore />} */}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </Draggable>
   );
 };
 
