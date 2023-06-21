3e4821dcaf79f84ca1cfd98a1c95880f7d5fad80
Saeed-Abedini <Saeed78Abedini@gmail.com>
2023-06-21 15:33:23 +0330

    change Slice and store name

--- TaskManager/src/services/features/boards/boardSlice.ts
+++ TaskManager/src/services/features/boards/boardSlice.ts
@@ -1,6 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import AXIOS from "../utils/AXIOS";
+import boardService from "./boardService";
+import changePositionReducer from "./reducers/changePositionReducer";
 
+type PositionProps = {
+  id: string;
+  index: number;
+};
 export type Task = {
   _id: string;
   name: string;
@@ -11,11 +16,10 @@ export type Task = {
   comments: [];
   position: number;
 };
-
 type BoardType = {
   _id: "";
   name: "";
-  position: "";
+  position: number;
   project: "";
   tasks: Task[];
 };
@@ -23,12 +27,12 @@ type ProjectType = {
   projectId: string;
   projectBoards: BoardType[];
 };
-type initialStateType = {
+export type initialStateType = {
   isLoading: boolean;
   isSuccess: boolean;
   isError: boolean;
   message: unknown;
-  selectedId: string;
+  selectedProjectId: string;
   projects: ProjectType[];
   test: any;
 };
@@ -40,19 +44,65 @@ const initialState: initialStateType = {
   isSuccess: false,
   isError: false,
   message: "",
-  selectedId: "",
+  selectedProjectId: "",
   projects: [],
   test: [],
 };
 
-// fetchBoards
+// fetch Boards
 const fetchBoards = createAsyncThunk(
   "Boards/fetchBoards",
   async (id: string, thunkAPI) => {
     try {
-      const response = await AXIOS.get(`/api/board/${id}`);
-      const data = await response.data;
-      return data;
+      return await boardService.fetchBoards(id);
+    } catch (error: any) {
+      const message =
+        error?.response?.data?.message || error.message || error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+// Change Board Position
+const fetchChangeBoardPosition = createAsyncThunk(
+  "Boards/fetchChangeBoardPosition",
+  async ({ id, index }: PositionProps, thunkAPI) => {
+    try {
+      return await boardService.fetchChangeBoardPosition({ id, index });
+    } catch (error: any) {
+      const message =
+        error?.response?.data?.message || error.message || error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+//  Change Task Position
+const fetchChangeTaskPosition = createAsyncThunk(
+  "Boards/ChangeTaskPosition",
+  async ({ id, index }: PositionProps, thunkAPI) => {
+    try {
+      return await boardService.fetchChangeTaskPosition({ id, index });
+    } catch (error: any) {
+      const message =
+        error?.response?.data?.message || error.message || error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+//  Change Task Board
+const fetchChangeTaskBoard = createAsyncThunk(
+  "Boards/ChangeTaskBoard",
+  async (
+    {
+      id,
+      boardId,
+    }: {
+      id: string;
+      boardId: string;
+    },
+    thunkAPI
+  ) => {
+    try {
+      return await boardService.fetchChangeTaskBoard({ id, boardId });
     } catch (error: any) {
       const message =
         error?.response?.data?.message || error.message || error.toString();
@@ -65,61 +115,13 @@ const boardsSlice = createSlice({
   name: "Boards",
   initialState,
   reducers: {
-    setSelectedId: (state, action) => {
-      state.selectedId = action.payload;
+    setSelectedProjectId: (state, action) => {
+      state.selectedProjectId = action.payload;
     },
-
-    changeTaskPosition: (state, action) => {
-      // Find the project that is currently active
-      const activeProject = state.projects.find((project) => {
-        return project.projectId == state.selectedId;
-      });
-
-      // Find the index of the active project in the list of projects
-      const activeProjectIndex = state.projects.findIndex((project) => {
-        return project.projectId == state.selectedId;
-      });
-
-      // Find the board where the task has been dropped
-      const droppableBoard = activeProject?.projectBoards.find((board) => {
-        return board._id == action.payload.source.droppableId;
-      });
-
-      // Find the index of the droppable board
-      const droppableBoardIndex = state.projects[
-        activeProjectIndex
-      ].projectBoards.findIndex((board) => {
-        return board._id == action.payload.source.droppableId;
-      });
-
-      // Create a copy of the tasks array for the current board
-      const items = Array.from(
-        state.projects[activeProjectIndex].projectBoards[droppableBoardIndex]
-          .tasks
-      );
-
-      // Find the task that was dragged and dropped
-      const reorderedTask = droppableBoard?.tasks.find((task) => {
-        return task._id == action.payload.draggableId;
-      });
-
-      // Get the destination and source index of the dragged task
-      const destinationPos = action.payload.destination.index;
-      const sourcePos = action.payload.source.index;
-
-      // Remove the dragged item from its original position in the array
-      const [reorderedItem] = items.splice(sourcePos, 1);
-
-      // Insert the dragged item at its new position in the array
-      items.splice(destinationPos, 0, reorderedItem);
-
-      // Update the tasks array for the board
-      state.projects[activeProjectIndex].projectBoards[
-        droppableBoardIndex
-      ].tasks = items;
+    changePosition: (state, action) => {
+      changePositionReducer(state, action);
     },
   },
-
   extraReducers: (builder) => {
     builder
       .addCase(fetchBoards.pending, (state) => {
@@ -129,23 +131,41 @@ const boardsSlice = createSlice({
       .addCase(fetchBoards.fulfilled, (state, action) => {
         state.isLoading = false;
         state.isSuccess = true;
-
         const projectId = action.meta.arg;
-        const projectIndex = state.projects.findIndex((item) => {
-          return item.projectId === projectId;
+        state.test = action.payload;
+        state.projects.push({
+          projectId,
+          projectBoards: action.payload.sort(
+            (b: Task, a: Task) => a.position - b.position
+          ),
         });
-        if (projectIndex >= 0) {
-          // Project exists, add the new board to its list of boards
-          state.projects[projectIndex].projectBoards = action.payload;
-        } else {
-          // Project doesn't exist, create a new index and add the new board with its own list of boards
-          state.projects.push({
-            projectId: projectId,
-            projectBoards: action.payload,
-          });
-        }
       })
+
       .addCase(fetchBoards.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.projects = [];
+        state.isError = true;
+        state.message = action.payload;
+      })
+
+      // Change Board Position
+      .addCase(fetchChangeBoardPosition.rejected, (state, action) => {
+        state.isError = true;
+        state.message = action.payload;
+      })
+
+      // Change Task Position
+      .addCase(fetchChangeTaskPosition.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.projects = [];
+        state.isError = true;
+        state.message = action.payload;
+      })
+
+      // Change Task Board
+      .addCase(fetchChangeTaskBoard.rejected, (state, action) => {
         state.isLoading = false;
         state.isSuccess = false;
         state.projects = [];
@@ -154,7 +174,12 @@ const boardsSlice = createSlice({
       });
   },
 });
-
+//TODO Clean Store
 export default boardsSlice.reducer;
-export const { setSelectedId, changeTaskPosition } = boardsSlice.actions;
-export { fetchBoards };
+export const { setSelectedProjectId, changePosition } = boardsSlice.actions;
+export {
+  fetchBoards,
+  fetchChangeBoardPosition,
+  fetchChangeTaskPosition,
+  fetchChangeTaskBoard,
+};
