2a57337b78b0c5d4136b8d62e34a5972bea6caae
amir996 <amir2041375@gmail.com>
2023-06-26 19:22:20 +0330

    modify getPersianDate function

--- TaskManager/src/components/taskInformation/getPersianDate.ts
+++ TaskManager/src/components/taskInformation/getPersianDate.ts
@@ -11,3 +11,14 @@ export default function getPersianDate(passedDate: string) {
   const formatter = new Intl.DateTimeFormat("fa-IR-u-ca-persian", options);
   return formatter.format(date);
 }
+
+export function getPersianDateWithOutTime(passedDate: string) {
+  const date = new Date(passedDate);
+
+  const options: Intl.DateTimeFormatOptions = {
+    month: "long",
+    day: "numeric",
+  };
+  const formatter = new Intl.DateTimeFormat("fa-IR-u-ca-persian", options);
+  return formatter.format(date);
+}
