6267a585a0a88938ac28871f484c88d908c4f85d
amir996 <amir2041375@gmail.com>
2023-06-26 19:23:28 +0330

    add deadline update functionality to TaskInfoHeaderLeft

--- TaskManager/src/components/taskInformation/TaskInfoHeaderLeft.tsx
+++ TaskManager/src/components/taskInformation/TaskInfoHeaderLeft.tsx
@@ -1,17 +1,51 @@
 import { GoPlay } from "react-icons/go";
 import Watchers from "../ui/Watchers";
 import CloseIcon from "../ui/Close";
-import getPersianDate from "./getPersianDate";
+import { BsCalendar3 } from "react-icons/bs";
+import { createPortal } from "react-dom";
+import QuckCalendar from "../modals/Large/QuckCalendar";
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../services/app/hook";
+import { fetchUpdateTask } from "../../services/app/store";
+import { getPersianDateWithOutTime } from "./getPersianDate";
+import getGregorianDate from "./getGregorianDate";
 
 type TaskInfoHeaderLeftProps = {
   handleCloseTaskInfo: () => void;
-  deadline: string;
+  deadline: string | undefined;
+  description: string;
+  name: string;
 };
 
 const TaskInfoHeaderLeft = ({
   handleCloseTaskInfo,
   deadline,
+  description,
+  name,
 }: TaskInfoHeaderLeftProps) => {
+  const [calendar, setCalendar] = useState({
+    modal: false,
+    value: "",
+  });
+  const handleCalendar = (modalState: boolean) => {
+    setCalendar({ ...calendar, modal: modalState });
+  };
+  // const deadlineChangeHandler = () => {};
+
+  const dispatch = useAppDispatch();
+  const { selectedTaskId } = useAppSelector((state) => state.boards);
+
+  const submitChangesHandler = (deadline: string) => {
+    dispatch(
+      fetchUpdateTask({
+        description,
+        name,
+        deadline,
+        taskId: selectedTaskId,
+      })
+    );
+  };
+
   return (
     <div className="w-1/2  h-full relative">
       <div className="w-full h-14 px-4  absolute bottom-6 flex justify-between items-center">
@@ -36,9 +70,18 @@ const TaskInfoHeaderLeft = ({
           </div>
           {/* Deadline */}
           <div className="h-full pr-7 ml-auto">
-            <span className="text-BBBBBB text-xs font-medium">ددلاین</span>
+            <div className="flex gap-2">
+              <span className="text-BBBBBB text-xs font-medium">ددلاین</span>
+              <BsCalendar3
+                onClick={() => handleCalendar(true)}
+                className="mb-1 cursor-pointer"
+              />
+            </div>
             <div className="text-1E1E1E text-base font-medium">
-              {deadline ? getPersianDate(deadline) : "تعریف نشده"}
+              {deadline
+                ? getPersianDateWithOutTime(getGregorianDate(deadline))
+                : "تعریف نشده"}
+              {/* {deadline} */}
             </div>
           </div>
         </div>
@@ -52,6 +95,14 @@ const TaskInfoHeaderLeft = ({
       <span onClick={handleCloseTaskInfo}>
         <CloseIcon classes={"absolute left-3 top-2 text-BDBDBD"} />
       </span>
+      {calendar.modal &&
+        createPortal(
+          <QuckCalendar
+            submitChangesHandler={submitChangesHandler}
+            handleCalendar={handleCalendar}
+          />,
+          document.body
+        )}
     </div>
   );
 };
