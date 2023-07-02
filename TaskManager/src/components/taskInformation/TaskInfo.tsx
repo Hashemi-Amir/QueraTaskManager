88c575f631917b53b81bc647af88118f5153eb24
Saeed-Abedini <Saeed78Abedini@gmail.com>
2023-07-02 23:12:46 +0330

    add live search for tasks in culomnView and handle minor issues

--- TaskManager/src/components/taskInformation/TaskInfo.tsx
+++ TaskManager/src/components/taskInformation/TaskInfo.tsx
@@ -28,12 +28,11 @@ const TaskInfo = ({ handleCloseTaskInfo, taskInfo }: TaskInfoProps) => {
     borderColor,
     title,
   } = taskInfo;
-  console.log('render TaskIfo');
   const dispatch = useAppDispatch();
   useEffect(() => {
     dispatch(setSelectedBoardId(board));
     dispatch(setSelectedTaskId(_id));
-  }, [ board, _id]);
+  }, [ dispatch,board, _id]);
 
   return (
     <TaskInfoCard>
